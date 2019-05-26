08b5c9f86bd2128dfde30cecf26bfb40cd7e19a8
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-05-26 20:10:34 +0200

    Minimal example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js + TypeScript App</h1>
-  </div>
+	<div id="app">
+		<board
+			:rows="10"
+			:cols="20"
+			v-model="arr"
+			:color="color"
+		>
+			<!-- <template v-slot="props">
+				{{props.data}}
+			</template> -->
+		</board>
+		<input type="color" v-model="color"/>
+	</div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Board from './components/Board.vue';
+export default Vue.extend({
+	data() {
+		return {
+			arr: [],
+			color: '#000000',
+		};
+	},
+	methods: {
+	},
+	components: {
+		Board,
+	},
+});
+</script>
+
+<style lang="scss">
+#app {
+	height: 400px;
+}
+</style>
